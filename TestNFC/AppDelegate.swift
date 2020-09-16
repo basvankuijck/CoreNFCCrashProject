c9f1fe7b23984cf1aecdc8cb87dcd6eb6e1c617b
Bas van Kuijck <bas@e-sites.nl>
2020-09-16 11:45:29 +0200

    all

---
+++ TestNFC/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  TestNFC
+//
+//  Created by Bas van Kuijck on 16/09/2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.becomeKey()
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
